fc6ec64ad723a990387ac45a82470bf2330a41a8
jsantore <jsantore@bridgew.edu>
2021-09-21 10:30:33 -0400

    up to adding unique account IDs

--- src/com/company/BankAccount.java
+++ src/com/company/BankAccount.java
@@ -2,7 +2,24 @@ package com.company;
 public class BankAccount {
     private double balance;
     private float interestRate;
+    private int accountID;
+    private static int nextId = 100;
 
+    public BankAccount(){
+        interestRate = 0.02f;
+        accountID = nextId;
+        nextId++;  //same as nextID = nextId + 1
+    }
+    public BankAccount(double initialBalance, float initialRate){
+        balance = initialBalance;
+        interestRate = initialRate;
+        accountID = nextId;
+        nextId += 1;
+    }
+
+    public int getAccountID(){
+        return accountID;
+    }
     public void deposit(double amount){
         balance += amount;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	    var myAccount= new BankAccount();
+	    var yourAccount = new BankAccount(2000, 0.05f);
 	    myAccount.deposit(1000);
 	    var newBalance = myAccount.addInterest();
-
+		var yourBalance = yourAccount.addInterest();
+		System.out.println("Your account with ID: " +yourAccount.getAccountID()+
+				" has "+ yourBalance + " after adding the interest.");
 	    var succeeded = myAccount.withdraw(2000);
 	    if(succeeded)
 	        System.out.println("You managed to withdraw successfully ");
 	    else
-	        System.out.println("You tried to withdraw too much, your balance is"+myAccount.checkBalance());
+	        System.out.println("You tried to withdraw too much, account with ID: " + myAccount.getAccountID()+
+					" has a balance of "+myAccount.checkBalance());
     }
 }
