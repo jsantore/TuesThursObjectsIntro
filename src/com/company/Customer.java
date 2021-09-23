f16d81dc1d86d57b5157f3c4436722e2864b59c8
jsantore <jsantore@bridgew.edu>
2021-09-23 10:12:01 -0400

    costumer mostly implemented

---
+++ src/com/company/Customer.java
@@ -0,0 +1,30 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Customer {
+    private int customerID;
+    private String name;
+    private ArrayList<BankAccount> accounts;
+
+    public Customer(String customerName, int taxID){
+        customerID = taxID;
+        name = customerName;
+    }
+
+    public boolean openAccount(double initialDeposit){
+        var newAccount = new BankAccount();
+        newAccount.deposit(initialDeposit);
+        var didSucceed = accounts.add(newAccount);
+        return didSucceed;
+    }
+
+    public int getId(){
+        return customerID;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+}
