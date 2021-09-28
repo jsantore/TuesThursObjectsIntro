810b286bed16295e9ae6034abefb8955ad3edac2
jsantore <jsantore@bridgew.edu>
2021-09-28 10:46:31 -0400

    finished

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -27,9 +27,12 @@ public class Bank {
                 case 3:
                     Optional<Customer> current = selectCustomer(menuReader);
                     if(current.isPresent())
-                        doCustomerMenu(menuReader);
+                        doCustomerMenu(menuReader, current.get());
                     else
                         System.out.println("No customer with that ID found");
+                    break;
+                case 4:
+                    doYearlyMaintenance();
                     break;
                 default:
                     System.out.println("Please choose one of the menu options");
@@ -37,8 +40,66 @@ public class Bank {
         }
     }
 
-    private void doCustomerMenu(Scanner menuReader) {
-        System.out.println("We will finish this next week");
+    private void doYearlyMaintenance() {
+        //for each account - call addInterest and then print account info
+        for (var currentAccount: allAccounts){
+            currentAccount.addInterest();
+            System.out.println("Account ID: "+ currentAccount.getAccountID() + " has balance of "
+                    + currentAccount.checkBalance());
+        }
+
+    }
+
+    private void doCustomerMenu(Scanner menuReader, Customer currentCustomer) {
+        while(true){
+            printCustomerMenu();
+            var customerChoice = menuReader.nextInt();
+            switch (customerChoice){
+                case 1:
+                    openCustomerAccount(menuReader, currentCustomer);
+                    break;
+                case 2:
+                    closeCustomerAccount(menuReader, currentCustomer);
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("Invalid Input, please choose one of the options listed");
+            }
+        }
+    }
+
+    private void closeCustomerAccount(Scanner menuReader, Customer currentCustomer) {
+        //ask the user what account number to close
+        System.out.print("Enter account number of account to close:");
+        var accountNum = menuReader.nextInt();
+        //call close account on the customer passing that number
+        Optional<BankAccount> accountToClose = currentCustomer.closeAccount(accountNum);
+        //if the close succeeded remove the account from allAccounts
+        if(accountToClose.isPresent()){
+            allAccounts.remove(accountToClose.get());
+        }
+    }
+
+    private void openCustomerAccount(Scanner menuReader, Customer currentCustomer) {
+        //ask the user how much money the starting deposit is
+        System.out.println("Creating new account....");
+        System.out.print("What is the initial deposit for the account: ");
+        var initialDeposit = menuReader.nextDouble();
+        //call open account on the customer
+        var newAccount = currentCustomer.openAccount(initialDeposit);
+        //add the new account to allAccounts
+        allAccounts.add(newAccount);
+    }
+
+    private void printCustomerMenu() {
+        System.out.println("****************************************");
+        System.out.println("What do you want to do with this Customer");
+        System.out.println("   [1] Open an account");
+        System.out.println("   [2] Close an account");
+        System.out.println("   [3] Return to Main menu");
+        System.out.println("****************************************");
+        System.out.print(" Enter choice: ");
     }
 
     private Optional<Customer> selectCustomer(Scanner reader) {
@@ -62,10 +123,13 @@ public class Bank {
     }
 
     private void printMenu() {
+        System.out.println("===================================================");
         System.out.println("what would you like to do next (select the number):");
         System.out.println("   [1] Exit the program");
         System.out.println("   [2] Add a customer");
         System.out.println("   [3] Select customer by ID");
+        System.out.println("   [4] Do the yearly maintenance and show all accounts");
+        System.out.println("===================================================");
         System.out.print("Enter choice:");
     }
 
--- src/com/company/Customer.java
+++ src/com/company/Customer.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Customer {
     private int customerID;
@@ -10,13 +11,26 @@ public class Customer {
     public Customer(String customerName, int taxID){
         customerID = taxID;
         name = customerName;
+        accounts = new ArrayList<BankAccount>();
     }
 
-    public boolean openAccount(double initialDeposit){
+    public BankAccount openAccount(double initialDeposit){
         var newAccount = new BankAccount();
         newAccount.deposit(initialDeposit);
         var didSucceed = accounts.add(newAccount);
-        return didSucceed;
+        return newAccount;
+    }
+
+    public Optional<BankAccount> closeAccount(int accountNumber){
+        for (var account: accounts){
+            if (account.getAccountID() == accountNumber){
+                System.out.println("Removing account with account ID" + accountNumber + " from Customer +" + name);
+                accounts.remove(account);
+                return Optional.of(account);
+            }
+        }
+        System.out.println("Account with account Number" + accountNumber + " is not " + name+"'s account");
+        return Optional.empty();
     }
 
     public int getId(){
